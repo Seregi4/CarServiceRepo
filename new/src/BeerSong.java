00ee392c3b1ac3f1f2546171d5bb2fc2a8489a6a
Seregi4 <Seregi4.87@gmail.com>
2021-04-18 18:09:52 +0300

    123

---
+++ new/src/BeerSong.java
@@ -0,0 +1,13 @@
+public class BeerSong {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 40; i++) {
+          //  System.out.println(i);
+            if (i==1){
+                System.out.println("one batle in stena");
+            }
+            else {
+                System.out.println("more with one butle");
+            }
+        }
+    }
+}
---
+++ new/src/CalcTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+public class CalcTest {
+
+    @org.junit.Test
+    public void resultateAdd() {
+        resultateAdd(3,7);
+
+    }
+
+    private void resultateAdd(int i, int i1) {
+    }
+}
